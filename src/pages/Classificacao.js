c0c6b04547a696b30c424fe44195d16f51bca9d5
André Ferreira <andrefpires1@gmail.com>
2020-08-21 02:04:26 -0300

    titulo + botao home

--- src/pages/Classificacao.js
+++ src/pages/Classificacao.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 class Classificacao extends React.Components {
   render () {
     return (
-      <div></div>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <Link data-testid="btn-go-home" to='/home'>Home</Link>
+      </div>
     )
   }
 }
