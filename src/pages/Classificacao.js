670cf0ad60926b861a4cd3dabc551d55624ee887
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-08-27 12:03:26 -0300

    Botão para Home

--- src/pages/Classificacao.js
+++ src/pages/Classificacao.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+// import FormLogin from '../components/FormLogin';
 
 class Classificacao extends React.Components {
-  render () {
+  render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
-        <Link data-testid="btn-go-home" to='/home'>Home</Link>
+        <Link to="/home">
+          <button data-testid="btn-go-home">Home</button>
+        </Link>
       </div>
-    )
+    );
   }
 }
 
